d2db8ac03032acb32615348d8ba83a33eec5a55f
Sadman Hafiz Shuvo <ug2102021@cse.pstu.ac.bd>
2026-07-15 11:03:13 +0600

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        long long sum1 = n * n;
-        long long sum2 = n * (n + 1);
-        return gcd(sum1, sum2);
+        return n;
     }
 };
