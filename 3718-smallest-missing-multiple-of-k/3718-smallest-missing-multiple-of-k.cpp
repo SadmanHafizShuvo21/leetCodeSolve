31b974bdbebeaadfbe818b525b94e7ea62f2d74b
Sadman Hafiz Shuvo <ug2102021@cse.pstu.ac.bd>
2026-08-25 20:26:41 +0600

    Time: 5 ms (15.61%), Space: 25.2 MB (24.36%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        set<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            s.insert(nums[i]);
+        }
+
+        int x = k;
+        while (s.count(x)) {
+            x += k;
+        }
+
+        return x;
+    }
+};
